cff02d93a40f89a19c7a97a9b7f3e41a23278230
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-09-25 19:24:23 +0300

    1

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -15,13 +15,10 @@ namespace Lesson9
 
             switch (_myEnum)
             {
-                case MyFirstEnum.A:
-                        Console.WriteLine (MyFirstEnum.A);
-                    break;
+                case MyFirstEnum.A: Console.WriteLine(MyFirstEnum.A); break;
                 case MyFirstEnum.B: Console.WriteLine(MyFirstEnum.B); break;
                 case MyFirstEnum.C: Console.WriteLine(MyFirstEnum.C); break;
                 case MyFirstEnum.D: Console.WriteLine(MyFirstEnum.D); break;
-                default: break;
             }
         }
         enum MyFirstEnum : byte //(значения с 0 до 255)
