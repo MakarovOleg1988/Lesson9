b6bbcdbec522600856e9bc9ea999dcfb28da7cef
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-09-25 19:23:16 +0300

    1

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -9,43 +9,30 @@ namespace Lesson9
             MyThirdMethod();
         }
 
-        static void MySecondMethod(MyFirstEnum _myEnum)
-        {
-            if (_myEnum == MyFirstEnum.A)
-            {
-
-            }
-
-            if (_myEnum == 0)
-            {
-
-            }
-        }
-
         static void MyThirdMethod()
         {
-            switch (MyFirstEnum)
+            var _myEnum = new MyFirstEnum();
+
+            switch (_myEnum)
             {
                 case MyFirstEnum.A:
-                    {
-
-                    }
+                        Console.WriteLine (MyFirstEnum.A);
                     break;
-                case MyFirstEnum.B: break;
-                case MyFirstEnum.C: break;
-                case MyFirstEnum.D: break;
+                case MyFirstEnum.B: Console.WriteLine(MyFirstEnum.B); break;
+                case MyFirstEnum.C: Console.WriteLine(MyFirstEnum.C); break;
+                case MyFirstEnum.D: Console.WriteLine(MyFirstEnum.D); break;
                 default: break;
             }
         }
-
         enum MyFirstEnum : byte //(значения с 0 до 255)
         {
             A = 99, //Можно назначать свои значения
-            B,
+            B = 20,
             C,
             D,
             E,
             F
         }
+
     }
 }
